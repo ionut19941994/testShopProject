0b89d670061f59035e5470f5c2de172906650bae
ionut19941994 <ionut19941994@yahoo.com>
2022-10-18 02:07:03 +0300

    updated code to review

--- src/test/java/objects/homePage.java
+++ src/test/java/objects/homePage.java
@@ -1,32 +1,94 @@
 package objects;
 
 import cucumber.Base;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
+import javax.lang.model.util.Elements;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static java.lang.Float.valueOf;
+
 public class homePage extends Base {
 
-    @FindBy(xpath = "//h2[@class='woocommerce-loop-product__title']")
-    protected List<WebElement> product;
+    By product2=By.xpath("//a[contains(text(),'Add to cart')]//parent::div");
 
-    @FindBy(xpath = "//ul[@class='products columns-3']//following-sibling::li")
-    protected List<WebElement> addItem;
+    public List<WebElement> products(){
+        return driver.findElements(product2);
+    }
+
+    By price=By.xpath("//td[@class='product-subtotal']//following-sibling::span");
+
+    public WebElement price(){
+        return driver.findElement(price);
+    }
+
+    By cartItem=By.xpath("//tr[@class='woocommerce-cart-form__cart-item cart_item']");
+
+    public List<WebElement> cartItems() {
+        return driver.findElements(cartItem);
+    }
+
+    By addToCart=By.name("add-to-cart");
+
+    public WebElement addToCart(){
+        return driver.findElement(addToCart);
+    }
+
+    By goToCart=By.xpath("//a[contains(text(),'Cart')]");
+
+    public WebElement goToCart(){
+        return driver.findElement(goToCart);
+    }
+
+    By goBackHome=By.xpath("//a[@rel='home']");
+
+    public WebElement goBackHome(){
+        return driver.findElement(goBackHome);
+    }
 
     public homePage(WebDriver driver){
         Base.driver =driver;
     }
 
+    public void navigateBackHome() {
+        goBackHome().click();
+    }
+
+    public void navigateToCart() {
+        goToCart().isDisplayed();
+        goToCart().click();
+    }
+
+    public int howManyItems() {
+        return cartItems().size();
+    }
+
+    public float lowestPrice() {
+        int l=Integer.parseInt(price().toString().replaceAll("$","").replaceAll(".00",""));
+        for( int i=0; i< cartItems().size();i++){
+             if(Integer.parseInt(price().toString().replaceAll("$","").replaceAll(".00",""))<l)
+                 l= Integer.parseInt(price().toString().replaceAll("$","").replaceAll(".00",""));
+        }
+        return l;
+    }
+
+
+
 
     public void selectRandomProductAndAddToCart() {
         Random random = new Random();
         Actions action = new Actions(driver);
-        int randomized = random.nextInt(product.size());
-        action.moveToElement(product.get(randomized)).click().perform();
+        int randomized = random.nextInt(products().size());
+        action.moveToElement(products().get(randomized)).click().perform();
+        addToCart().click();
+
     }
 }
--- src/test/resources/features/homepage.feature
+++ src/test/resources/features/homepage.feature
@@ -3,10 +3,14 @@ Feature: Make various actions with the items in the shop
   Background: Customer navigates to the shop page
     Given that the user navigated to the Shop page
   @test
-  Scenario: Make various actions on the website
-    Given I add four random items to my cart
+  Scenario Outline: Make various actions on the website
+    Given I add <number> random items to my cart
     When I view my cart
-    Then I find the total four items listed in my cart
+    Then I find the total <number> items listed in my cart
     When I search for lowest price item
     And I am able to remove the lowest price item from my cart
     Then I am able to verify three items in my cart
+
+    Examples:
+    |number|
+    |4     |
